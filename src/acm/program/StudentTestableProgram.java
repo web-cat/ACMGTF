2235e552bc98396aabc22924239c1e22e74144e8
fr3lm0 <>
2010-02-10 21:53:38 +0000

    updated to new naming convention for methods

--- src/acm/program/StudentTestableProgram.java
+++ src/acm/program/StudentTestableProgram.java
@@ -3,6 +3,9 @@ package acm.program;
 import java.awt.Component;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
+
+
 
 import junit.framework.Assert;
 
@@ -10,21 +13,33 @@ import abbot.finder.BasicFinder;
 import abbot.finder.ComponentNotFoundException;
 import abbot.finder.Matcher;
 import abbot.finder.MultipleComponentsFoundException;
+import acm.gui.IntField;
+import acm.gui.DoubleField;
 
 public class StudentTestableProgram extends TestableProgram
 {
-
-    public void assertExistsJButtonWithText(String buttonText)
+    public void assertHasJButtonWithText(String buttonText)
     {
-        assertHasJButtonWithText("No button with label " + buttonText + " found.", buttonText);
+        assertHasGUIObject("No button with text: " + buttonText + " found.", GUIFilter.withText(buttonText).and(GUIFilter.type(JButton.class)));
     }
     
-    public void assertExistsJLabelWithText(String labelText)
+    public void assertHasJLabelWithText(String labelText)
     {
-        assertHasJLabelWithText("No label with text " + labelText + " found.", labelText);
+        assertHasGUIObject("No label with text: " + labelText + " found.", GUIFilter.withText(labelText).and(GUIFilter.type(JLabel.class)));
+    } 
+    
+    public void assertHasIntFieldWithText(String fieldText)
+    {
+        assertHasGUIObject("No IntField with text: " + fieldText + " found.", GUIFilter.withText(fieldText).and(GUIFilter.type(IntField.class)));
     }
     
+    public void assertHasDoubleFieldWithText(String fieldText)
+    {
+        assertHasGUIObject("No IntField with text: " + fieldText + " found.", GUIFilter.withText(fieldText).and(GUIFilter.type(DoubleField.class)));
+    }
     
-    
-    
+    public void assertHasGUIObject(GUIFilter filter)
+    {
+        assertHasGUIObject("No object found that matched specified filter", filter);
+    }
 }
